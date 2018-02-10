9e4342f0f523aa33860c5442198deb886ec0952d
Felfit <31731191+Felfit@users.noreply.github.com>
2018-02-10 16:12:35 +0000

    Adicionado mapa

--- Temp/code.js
+++ Temp/code.js
@@ -14,32 +14,149 @@ var map = new mapboxgl.Map({
     zoom: 16.5, // starting zoom
     maxBounds: bounds
 });
-
 var geojson = {
   type: 'FeatureCollection',
   features: [{
     type: 'Feature',
     geometry: {
       type: 'Point',
-      coordinates: [-8.397372, 41.561699], 
+      coordinates: [-8.395659, 41.560356], 
     },
     properties: {
       title: 'Mapbox',
-      description: 'DI'
+      description: 'Complexo Pedagógico 1'
     }
+  },
+  {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.397723, 41.560088], 
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Complexo Pedagógico 2'
+      }
+  },
+  {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.397997, 41.560820], 
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Complexo Pedagógico 3'
+      }
   },
   {
     type: 'Feature',
     geometry: {
       type: 'Point',
-      coordinates: [-122.414, 37.776]
+      coordinates: [-8.396735, 41.560020]
     },
     properties: {
       title: 'Mapbox',
-      description: 'San Francisco, California'
+      description: 'Biblioteca Geral | Escola Superior de Enfermagem'
     }
+  },
+  {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.396806, 41.560328], 
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Instituto das Artes e Ciencias Humanas'
+      }
+  },
+  {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.396318, 41.561140], 
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Escola de Ciencias'
+      }
+  },
+  {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.397168, 41.561704], 
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Escola de Engenharia'
+      }
+  },
+  {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.398429, 41.561474], 
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Escola de Economia e Gestao'
+      }
+  },
+  {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.394953, 41.561918], 
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Complexo Desportivo da Universidade do Minho'
+      }
+  },
+  {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.394276, 41.562825], 
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Serviços Técnicos'
+      }
+  },
+  {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.397997, 41.560820], 
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Complexo Pedagógico 3'
+      }
+  },
+  {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.397997, 41.560820], 
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Complexo Pedagógico 3'
+      }
   }]
 };
+
+map.dragRotate.disable();
+
+var popup = new mapboxgl.Popup({closeOnClick: false})
+    .setLngLat([-8.3977517, 41.5607746])
+    .setHTML('<h1>Hello World!</h1>')
+    .addTo(map);
+
 
 // add markers to map
 geojson.features.forEach(function(marker) {
---
+++ Temp/maker.js
@@ -0,0 +1,135 @@
+var geojson = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.395659, 41.560356], 
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Complexo Pedagógico 1'
+      }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-8.397723, 41.560088], 
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Complexo Pedagógico 2'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-8.397997, 41.560820], 
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Complexo Pedagógico 3'
+        }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.396735, 41.560020]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Biblioteca Geral | Escola Superior de Enfermagem'
+      }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-8.396806, 41.560328], 
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Instituto das Artes e Ciencias Humanas'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-8.396318, 41.561140], 
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Escola de Ciencias'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-8.397168, 41.561704], 
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Escola de Engenharia'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-8.398429, 41.561474], 
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Escola de Economia'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-8.394953, 41.561918], 
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Complexo Desportivo da Universidade do Minho'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-8.394276, 41.562825], 
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Serviços Técnicos'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-8.397997, 41.560820], 
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Complexo Pedagógico 3'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-8.397997, 41.560820], 
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Complexo Pedagógico 3'
+        }
+    }]
+  };
