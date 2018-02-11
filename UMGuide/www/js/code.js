4022d7b53b9d6557059b8d82f3822b3b2e05e027
Carlos Castro <cpac20.4@gmail.com>
2018-02-11 05:59:40 +0000

    Adds bares markers and button

--- UMGuide/www/js/code.js
+++ UMGuide/www/js/code.js
@@ -55,10 +55,30 @@ geojson.features.forEach(function(marker,i) {
   .addTo(map);
 });
 
+bares.forEach(function(marker, i) {
+  var el = document.createElement('img');
+  el.className = 'bares';
+  el.src = "img/bar.png";
+
+  new mapboxgl.Marker(el)
+  .setLngLat(bares[i])
+  .addTo(map);
+});
+
 function findMe() {
   navigator.geolocation.getCurrentPosition(function(pos){
     createMarker([pos.coords.longitude, pos.coords.latitude]);
   });
+}
+
+function toggleBares(elemClicked) {
+	$('.bares').toggle();
+	var notColored = $(elemClicked).hasClass('notColored');
+	if(notColored){
+		$(elemClicked).removeClass('notColored');
+	} else {
+		$(elemClicked).addClass('notColored');
+	}
 }
 
 function createMarker(coord) {
