ee6b371bc6182d21e5c0593cf267f1af05f50a2c
CesarAugustoBorges <cesar.a.borges98@gmail.com>
2018-02-11 07:13:42 +0000

    Added events page

---
+++ UMGuide/www/js/event.js
@@ -0,0 +1,33 @@
+// Add repositories to the .projects
+function getEvents(){
+	$.get(`https://felfit.github.io/fakedatabase/data.json`, function(data, status){
+		if(status=='success'){
+      var html = "";
+			for (i in data.events) {
+        html+="<div class=\"event container\">"
+        html+= "<h3 class=\"title\">" + data.events[i].name +'</h3>'
+        html+= "<div class=\"row\">"
+        html+= "<div class=\"col-sm-3\">"
+        html+= "<img class=\"event-image\" src=\""+data.events[i].img+"\" alt=\"Event\" height=\"50\" width=\"auto\" style=\"float: center;\">"
+        html+= "</div>"
+        html+= "<div class=\"col-sm-6\">"
+        html+= "<p class=\"event-description\">"+ data.events[i].description+"</p>"
+        html+= "</div>"
+        html+= "<p><a href=\""+data.events[i].website+"\" target=\"_blank\" > Website </a> </p>"
+        html+="</div>"
+      }
+		}
+    html+="<div id=\"event\">No more events.</div>"
+    $('#events').append(html);
+	});
+}
+
+function openEventPanel(){
+  $('#map').hide();
+  $('#events').show();
+}
+
+function openMap(){
+  $('#map').show();
+  $('#events').hide();
+}
